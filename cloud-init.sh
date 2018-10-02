4d6b9d9208c067aa9baab837b9051b882d32794b
tiagoReichert <tiago_reichert@hotmail.com.br>
2018-10-02 16:13:59 -0300

    Added cloud-init with Docker installation and NodeJS service creation

---
+++ cloud-init.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Install dependencies and add Docker repository to apt
+sudo apt-get update
+sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+sudo apt-get update
+
+# Install Docker community edition
+sudo apt-get install -y docker-ce
+
+# Enable swarm mode
+sudo docker swarm init
+
+# Deploy swarm service with NodeJS (quantity of replicas is the same as processors available on the host)
+sudo docker service create --name nodejs --replicas=$(nproc) --publish 3000:3000 tiagoreichert/cloud-challenge
+
+
