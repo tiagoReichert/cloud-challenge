e881a932b2a990bb724e4549c608241db2931967
tiagoReichert <tiago_reichert@hotmail.com.br>
2018-10-03 16:15:51 -0300

    Added docker.sock volume to logparser container

--- cloud-init.sh
+++ cloud-init.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+# Specify here the needed parameters:
+export SERVICE_NAME='nodejs'
+export SMTP_SERVER='smtp.gmail.com'
+export SMTP_PORT=587
+export SMTP_TLS=True
+export SMTP_NAME='CloudChallenge'
+export SMTP_USERNAME=''
+export SMTP_PASSWORD=''
+export SMTP_RECIPIENT=''
+
 # Install dependencies and add Docker repository to apt
 sudo apt-get update
 sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common
@@ -9,6 +19,7 @@ sudo add-apt-repository \
    $(lsb_release -cs) \
    stable"
 sudo apt-get update
+sudo systemctl enable docker
 
 # Install Docker community edition
 sudo apt-get install -y docker-ce
@@ -17,6 +28,10 @@ sudo apt-get install -y docker-ce
 sudo docker swarm init
 
 # Deploy swarm service with NodeJS (quantity of replicas is the same as processors available on the host)
-sudo docker service create --name nodejs --replicas=$(nproc) --publish 3000:3000 tiagoreichert/cloud-challenge:1.0
+sudo docker service create --name nodejs --replicas=$(nproc) --publish 3000:3000 tiagoreichert/cloud-challenge-nodejs:1.0
 
-
+# Deploy log_parser that will run every day at 22:00
+sudo docker service create --name logparser -e SMTP_SERVER=${SMTP_SERVER} -e SMTP_PORT=${SMTP_PORT} \
+-e SMTP_TLS=${SMTP_TLS} -e SMTP_NAME=${SMTP_NAME} -e SMTP_USERNAME=${SMTP_USERNAME} -e SMTP_PASSWORD=${SMTP_PASSWORD} \
+-e SMTP_RECIPIENT=${SMTP_RECIPIENT} -e SERVICE_NAME=${SERVICE_NAME} -v /var/run/docker.sock:/var/run/docker.sock \
+tiagoreichert/cloud-challenge-logparser
