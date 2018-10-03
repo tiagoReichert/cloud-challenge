0abecca09a3b42b52b43dfa3b0c7b71359b60ba9
tiagoReichert <tiago_reichert@hotmail.com.br>
2018-10-03 16:57:32 -0300

    Changed NodeJS app to version 2

--- nodejs/server.js
+++ nodejs/server.js
@@ -5,7 +5,7 @@ var morgan  = require('morgan')
 app.use(morgan('combined'))
 
 app.get('/', function (req, res) {
-res.send('Hello World!');
+res.send('Hello World! (Version 2)');
 });
 
 app.listen(3000, function () {
