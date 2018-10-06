f7d7e0d7aca22530b91cc6539493007df031af96
tiagoReichert <tiago_reichert@hotmail.com.br>
2018-10-05 21:54:46 -0300

    Added environment variable with quantity of nginx workers

--- load_balancer/entrypoint.sh
+++ load_balancer/entrypoint.sh
@@ -2,6 +2,9 @@
 
 openssl req -subj "/CN=${COMMON_NAME}" -x509 -newkey rsa:4096 -nodes -keyout /etc/nginx/conf.d/key.pem -out /etc/nginx/conf.d/cert.pem -days 365
 
+# replaces NODEJS_ADDRESS with environment variable on default.conf
 sed -i "s/{NODEJS_ADDRESS}/${NODEJS_ADDRESS}/" /etc/nginx/conf.d/default.conf
+# replaces NGINX_WORKERS with environment variable on nginx.conf
+sed -i "s/{NGINX_WORKERS}/${NGINX_WORKERS}/" /etc/nginx/nginx.conf
 
 nginx -g 'daemon off;'
